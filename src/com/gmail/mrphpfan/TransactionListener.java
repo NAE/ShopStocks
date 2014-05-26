c7a1d127123c4df9c3e432b56badf0dfc9f62876
NAE <nileproject@hotmail.com>
2014-05-26 17:30:28 -0600

    Additions to essentials sell-sign interaction detection

--- src/com/gmail/mrphpfan/TransactionListener.java
+++ src/com/gmail/mrphpfan/TransactionListener.java
@@ -1,13 +1,21 @@
 package com.gmail.mrphpfan;
 
+import net.ess3.api.events.SignInteractEvent;
+
 import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import com.Acrobot.ChestShop.Events.TransactionEvent;
 import com.Acrobot.ChestShop.Events.TransactionEvent.TransactionType;
+import com.earth2me.essentials.IUser;
+import com.earth2me.essentials.signs.EssentialsSign.ISign;
+import com.earth2me.essentials.signs.SignSell;
 
 public class TransactionListener implements Listener {
 	
@@ -18,8 +26,12 @@ public class TransactionListener implements Listener {
 		this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 	
+	/**
+	 * This is used for detecting sell-to AdminShops
+	 * @param e
+	 */
 	@EventHandler
-	public void onPlayerTransaction(TransactionEvent e){
+	private void onPlayerTransaction(TransactionEvent e){
 		String shopOwnerName = e.getOwner().getName();
 		Player client = e.getClient();
 		//we only care if it is AdminShop
@@ -44,5 +56,37 @@ public class TransactionListener implements Listener {
 		
 	}
 	
+	/**
+	 * Detect when user interacts with essentials signs
+	 * @param e
+	 */
+	@EventHandler
+	private void onEssentialSignInteract(SignInteractEvent e){
+		IUser user = e.getUser();
+		//check if this is a sell sign
+		if(e.getEssentialsSign().getClass().equals(SignSell.class)){
+			ISign sign = e.getSign();
+			//get the quantity, material, and price
+			int quantity = Integer.parseInt(sign.getLine(1));
+			Material itemMaterial = Material.valueOf(sign.getLine(2));
+			//strip the dollar sign off the front of the last line
+			double price = Double.parseDouble(sign.getLine(3).substring(1));
+			
+			double total = quantity * price;
+			
+		}
+	}
+	
+	/**
+	 * Returns true if given string is an integer, false if not
+	 */
+	private boolean isInteger(String str){
+		try{
+			Integer.parseInt(str);
+		}catch(NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
 	
 }
